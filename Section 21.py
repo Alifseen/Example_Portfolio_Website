022a120c0d2b422471434eb9ff7239b17df57192
Alifseen <alifseen@gmail.com>
2024-10-16 10:35:02 +0500

    Created columns, set placeholder elements, set page width to wide

--- Section 21.py
+++ Section 21.py
@@ -1,1 +1,19 @@
+"""Portfolio Website
+- This app wont have a backend since we are not processing data through code, we already have the data in csv and we are only displaying it
 
+"""
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+left, right = st.columns(2, vertical_alignment="top", gap="medium", )
+
+## Introduction Element Left Column
+left.image("Files/Images/photo.png")
+
+## Introduction Element Right Column
+right.title("Name Here")
+description = """
+Description here
+"""
+right.info(description)
