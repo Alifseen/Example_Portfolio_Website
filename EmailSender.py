a017cd6811cdf0afa6985f0109c744b811b7854f
Alifseen <96657999+Alifseen@users.noreply.github.com>
2024-11-19 13:47:05 +0500

    Update EmailSender.py

--- EmailSender.py
+++ EmailSender.py
@@ -8,15 +8,15 @@ import os  ## 11. We import os standard module to make an environment variable
 ## 10. We convert this code into a function, but we need to add parameters manually.
 def send_email(subject, body):
     ## 2. We then save the username and password in variables
-    username = "alifseen@gmail.com"
-    password = os.getenv("EMAILSENDERPASS")  ## 12. It is not safe to store sensitive information in a script, best way to do it is to store it in an environment variable on your computer and then simply call it here by making sure the name is same. in this case, "EMAILSENDERPASS"
+    username = "username here"
+    password = os.getenv("env variable here")  ## 12. It is not safe to store sensitive information in a script, best way to do it is to store it in an environment variable on your computer and then simply call it here by making sure the name is same. in this case, "EMAILSENDERPASS"
     ## 3. We save email service providers smtp server address and port
     emailHost = "smtp.gmail.com"
     hostPort = 465
     ## 4. We create an ssl certificate
     sslContext = ssl.create_default_context()
     ## 5. Specify receiver
-    receiver = "mayfairenergysolutions@gmail.com"
+    receiver = "receiver email here"
     ## 6. specify message
     message = f"""Subject: {subject}
 {body} 
